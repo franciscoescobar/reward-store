3d3d424e11fef2377d8c3fae2ee3e12f50be1585
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-07-23 16:15:15 -0300

    Updates

---
+++ precache-manifest.9dbbf2f10528455bae6b6489449af65a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb01e93ff8ff94a8ea440ba214f3984",
+    "url": "/reward-store/index.html"
+  },
+  {
+    "revision": "7059fa5770bb346d16e2",
+    "url": "/reward-store/static/css/main.c4738a26.chunk.css"
+  },
+  {
+    "revision": "553f0e842e8d3e38589a",
+    "url": "/reward-store/static/js/2.2f1d0604.chunk.js"
+  },
+  {
+    "revision": "7059fa5770bb346d16e2",
+    "url": "/reward-store/static/js/main.fc6990a0.chunk.js"
+  },
+  {
+    "revision": "4cedea5e5c431a4d83f4",
+    "url": "/reward-store/static/js/runtime~main.2893dc18.js"
+  },
+  {
+    "revision": "71af6f4be21b71f7b37a986aca3a5af6",
+    "url": "/reward-store/static/media/aerolab-logo.71af6f4b.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/reward-store/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/reward-store/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/reward-store/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/reward-store/static/media/header-x1.f0b98264.png"
+  }
+]);
